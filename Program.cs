f5aba66fb1cf05be9d36506faa5ab9a022e03995
Revan Arturito <revan.arturito@gmail.com>
2024-03-17 11:00:52 +0700

    Menambahkan ClassGeneric

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+internal class Program
+{
+    public class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Data yang tersimpan adalah : {data}");
+        }
+    };
+
+    private static void Main(string[] args)
+    {
+        Console.Write("NIM : ");
+        String NIM = Console.ReadLine();
+        DataGeneric<string> data = new DataGeneric<string>(NIM);
+        data.PrintData();
+    }
+};
+
+
